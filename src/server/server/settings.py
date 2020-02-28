7616fa59e7a4b5f2bbadcadb244f79ba863b4c92
Janne Sarja <jsarja1@gmail.com>
2020-02-28 15:56:34 +0200

    Database: Setup SQLite database and create model for testing that the database works.

--- src/server/server/settings.py
+++ src/server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'server.apps.stockdata',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'stock_price_sandbox_db.sqlite3',
     }
 }
 
