a01485b5ad8ee6bf11419c41d0787636090102b7
Janne Sarja <jsarja1@gmail.com>
2020-03-19 09:32:54 +0200

    Server Stock Data: Create URL end point and view function for Alpha VantageAPI key and implement GET and PUT methods.

--- src/server/server/apps/stockdata/urls.py
+++ src/server/server/apps/stockdata/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test', views.test, name='test'),
+    path('av_api_key', views.av_api_key, name='av_api_key'),
+    path('search_stock', views.search_stock, name='search_stock'),
+
+    path('daily_data/stock_price_plot', views.search_stock, name='search_stock'),
+
 ]
--- src/server/server/apps/stockdata/views.py
+++ src/server/server/apps/stockdata/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from server.apps.authentication.models import APIUser
 
-def test(request):
-    return HttpResponse("test")
+@api_view(['GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def av_api_key(request):
+    try:
+        user = APIUser.objects.get(user=request.user)
+    except APIUser.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        return Response({"key": user.alpha_vantage_api_key})
+
+    if request.method == 'PUT':
+        if "alpha_vantage_api_key" not in request.data:
+            return Response("Please provide new alpha_vantage_api_key value " + 
+                "in request's body", status=status.HTTP_400_BAD_REQUEST)
+        
+        user.alpha_vantage_api_key = request.data["alpha_vantage_api_key"]
+        user.save()
+        return Response({"key": user.alpha_vantage_api_key})
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def search_stock(request):
+    content = {
+        'status': 'request was permitted'
+    }
+    return Response(content)
